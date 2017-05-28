9a0aa27dffae35c5c1c36777b0092b1696194da8
Shane McCulley <smcculle@uno.edu>
2017-05-28 14:26:30 -0500

    Added mapping from drive type to log key

--- gsuite/__init__.py
+++ gsuite/__init__.py
@@ -5,7 +5,9 @@ API_BASE = 'https://docs.google.com/{drive}/d/{file_id}/{params}'
 REV_PARAMS = 'revisions/load?start={start}&end={end}'
 RENDER_PARAMS = 'renderdata?id={file_id}'
 MIME_TYPE = "mimeType = 'application/vnd.google-apps.{}'"
-SERVICES = ['document']  # , 'drawing', 'form', 'presentation', 'spreadsheet']
+SERVICES = ['document', 'spreadsheet']  # , 'drawing', 'form', 'presentation', 'spreadsheet']
+LOG_DRIVE = {'document': 'document', 'spreadsheet': 'spreadsheets', 'drawing': 'drawings',
+             'presentation': 'presentation', 'form': 'forms'}
 DRAW_PARAMS = 'image?w={w}&h={h}'
 CHUNKED_ORDER = ['si', 'ei', 'st']
 
