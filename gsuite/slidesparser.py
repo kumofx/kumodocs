76537e05b9ba7a4ca701aeee85c6609e8ec17edb
Shane McCulley <smcculle@uno.edu>
2017-07-15 14:16:38 -0500

    Fixed #24
    * Added SlidesLine class
    * Refactored image logic into SlidesLine
    * Created descriptive boolean functions for slide images
    * Updated docstrings

--- gsuite/slidesparser.py
+++ gsuite/slidesparser.py
@@ -25,6 +25,61 @@ def log_msg(cls, msg, error_level):
 
 
 class SlidesParser(object):
+    class SlidesLine(object):
+        """ Abstraction of a Slides revision log entry along with methods required to retrieve objects"""
+
+        def __init__(self, line):
+            self.line = line
+
+        @property
+        def image_id(self):
+            """ Returns image_id associated with image insertion """
+            try:
+                image_section = self.line[0][1][0][4]
+            except IndexError:
+                _image_id = None
+            else:
+                if self.is_url_src():
+                    if self.not_drive_src():
+                        id_index = 7  # self.line[0][1][0][4][7]
+                    else:
+                        id_index = 9  # _image_id = self.line[0][1][0][4][9]
+                else:  # uploaded by url, source in [][]..[9], image_id in [11]
+                    id_index = 11  # _image_id = self.line[0][1][0][4][11]
+
+                _image_id = image_section[id_index]
+            return _image_id
+
+        @property
+        def slide_id(self):
+            """ Returns slide_id associated with object insertion """
+            try:
+                _slide_id = self.line[0][1][0][5]
+            except IndexError:
+                _slide_id = None
+
+            return _slide_id
+
+        def has_multiset(self):
+            """ Returns true if line contains a multiset"""
+            return self.line[0][0] == 4
+
+        def has_insert_section(self):
+            """ Returns true if line contains image insert section"""
+            return self.line[0][1][1][0] == 44
+
+        def has_vid_insert(self):
+            """ Returns true if line contains a video insert section"""
+            return len(self.line[0][1][0][4]) == 18
+
+        def is_url_src(self):
+            """ Returns true if image source is from url """
+            return type(self.line[0][1][0][4][11]) == list
+
+        def not_drive_src(self):
+            """ Returns true if image source is not from drive """
+            return type(self.line[0][1][0][4][9]) == list
+
     def __init__(self, client, KumoObj, delimiter='|'):
         self.log = None
         self.flat_log = None
@@ -39,6 +94,13 @@ class SlidesParser(object):
         self.pt_parser = PlainTextParser(self.KumoObj)
 
     def get_log(self, start, end, choice):
+        """
+        Retrives revision log based on parameters
+        :param start: Starting log revision
+        :param end: Ending log revision
+        :param choice: gsuite.FileChoice object containing choice metadata such as file_id
+        :return:
+        """
         log_msg(self, "Retrieving revision log", "info")
         log_url = self.client.create_log_url(start=start, end=end, choice=choice)
         response, log = self.client.request(url=log_url)
@@ -50,7 +112,7 @@ class SlidesParser(object):
             raise gsuite.InvalidLogFormat('Check gsuite.LOG_START_CHR and compare to beginning of log')
 
     def recover_objects(self, log, flat_log, choice):
-        """ Recovers plain text, comments, images, drawings, and suggestions from flat_log. 
+        """ Recovers plain text, comments, images, drawings, and suggestions from the log
         :return: A list of recovered objects as KumoObj
         """
 
@@ -67,6 +129,7 @@ class SlidesParser(object):
         return objects
 
     def get_plain_text(self, log):
+        """ Returns a list of KumoObj containing plain-text content for each text box in the presentation"""
 
         log_msg(self, 'Recovering plain text', 'info')
         return self.pt_parser.get_plain_text(log)
@@ -84,29 +147,23 @@ class SlidesParser(object):
 
         log_msg(self, 'Retrieving images', 'info')
         image_ids = {}
-        for line in log['changelog']:
-            # line[0][0] is action type, 4 is multiset, 44 is insert image action
-            # for video inserts, len...[4] is 18; exclude video inserts for now
-            if line[0][0] == 4 and line[0][1][1][0] == 44 and len(line[0][1][0][4]) != 18:
-                # for drive,personal upload, image id in ...[9], else if url in ...[11]
-                slide_id = line[0][1][0][5]
-                # if ..[11] is a list, the image was not uploaded via url
-                if type(line[0][1][0][4][11]) == list:
-                    # if ..[9] is a list, not uploaded by drive
-                    if type(line[0][1][0][4][9]) == list:
-                        image_id = line[0][1][0][4][7]
-                    else:
-                        image_id = line[0][1][0][4][9]
-                else:
-                    # if 11 is not a list, it was uploaded by url, src in ...[9]
-                    image_id = line[0][1][0][4][11]
+        for line in (self.SlidesLine(entry) for entry in log['changelog']):
 
-                # image_ids[slide_id].append(image_id)
+            # exclude video inserts for now
+            if line.has_multiset() and line.has_insert_section() and not line.has_vid_insert():
+                slide_id = line.slide_id
+                image_id = line.image_id
                 image_ids[image_id] = slide_id
 
         return image_ids
 
     def create_obj_list(self, objects, type_):
+        """
+        Creates a list of KumoObj of the appropriate type adn content
+        :param objects: A list of objects with a content property
+        :param type_: object type is prepended to the filename
+        :return: List of KumoObj
+        """
         obj_list = []
         for i, obj in enumerate(objects):
             filename = '{}{}{}'.format(type_, i, obj.extension)
@@ -139,6 +196,7 @@ class SlidesParser(object):
 
 
 class PlainTextParser(object):
+    """ Returns a list of KumoObj containing plain-text for each text box for each slide"""
     def __init__(self, KumoObj):
         self.KumoObj = KumoObj
 
@@ -186,11 +244,21 @@ class Presentation(object):
         return log['changelog'][1:]
 
     def parse(self, data):
+        """
+        Sends each line to be parsed
+        :param data: Changelog portion of revision log
+        :return: None
+        """
         for entry in data:
             line = entry[0]
             self.parse_line(line)
 
     def parse_line(self, line):
+        """
+        Calls appropriate function based on action type in each line
+        :param line: A single entry in the changelog
+        :return: None
+        """
         action = line[0]
         if action in self.functions:
             func = self.functions[action]
