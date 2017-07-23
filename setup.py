dfc960ce04ba05c250b8a25a4201ce09b79c13c3
Shane McCulley <smcculle@uno.edu>
2017-07-22 20:06:38 -0500

    Updates version to 0.2, adds nose

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Kumodocs',
-    version='0.1',
+    version='0.2',
     packages=['', 'gsuite'],
     url='https://github.com/kumofx/kumodocs',
     license='Apache License 2.0',
@@ -11,7 +11,7 @@ setup(
     description='A tool for GSuite acquisition and analysis that can be easily extended through module creation '
                 'to other services. ',
     data_files=[('config', ['config/cfg', 'config/gdrive_config.json'])],
-    install_requires=['click', 'google-api-python-client'],
+    install_requires=['click', 'google-api-python-client', 'nose'],
     entry_points='''
         [console_scripts]
         kumodocs=kumodocs:cli
