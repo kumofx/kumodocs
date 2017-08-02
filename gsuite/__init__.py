af39cb5b3d5ac6790466a6386bdb425668755244
Shane McCulley <smcculle@uno.edu>
2017-08-02 15:03:19 -0500

    Refs #43 -- Moves SERVICES to GSuiteDriver
    Adds MIME_TYPE, CAN_EDIT, and Q_PARAM to support advanced filtering.

--- gsuite/__init__.py
+++ gsuite/__init__.py
@@ -1,14 +1,15 @@
 import logging
 from collections import namedtuple
 
-# package-level constants defined below
+# package-level constants
 API_BASE = 'https://docs.google.com/{drive}/d/{file_id}/{params}'
 REV_PARAMS = 'revisions/load?start={start}&end={end}'
 RENDER_PARAMS = 'renderdata?id={file_id}'
-MIME_TYPE = "mimeType = 'application/vnd.google-apps.{}'"
-SERVICES = ['document', 'presentation', 'spreadsheet', 'drawing']
 LOG_DRIVE = {'document': 'document', 'spreadsheet': 'spreadsheets', 'drawing': 'drawings',
              'presentation': 'presentation', 'form': 'forms'}
+MIME_TYPE = "mimeType = 'application/vnd.google-apps.{drive_type}'"
+CAN_EDIT = "'{email}' in writers"
+Q_PARAM = '{0} and {1}'.format(MIME_TYPE, CAN_EDIT)
 DRAW_PARAMS = 'image?w={w}&h={h}'
 CHUNKED_ORDER = ['si', 'ei', 'st']
 LOG_START_CHR = ")]}'\n"
